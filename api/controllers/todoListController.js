f50bfc6c8074ea8841dc71451be260ae2d136b0d
sebasexy <spider_cbaz_10@hotmail.com>
2018-04-25 22:16:06 -0500

    Almost done

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -12,6 +12,22 @@ exports.list_all_tasks = function(req, res) {
   });
 };
 
+exports.list_all_tasks_from_user = function(req, res){
+  Task.find({email : req.query.email}, function(err, task){
+    if (err)
+        res.send(err);
+    res.json(task);
+  });
+};
+
+exports.list_all_pending_tasks_from_user = function(req, res){
+    Task.find({$or: [ {status: 'pending'}, {status : 'late'}], email: req.query.email}, function(err, task){
+      if(err)
+        res.send(err);
+      res.json(task);
+    });
+};
+
 
 exports.create_a_task = function(req, res) {
   var new_task = new Task(req.body);
@@ -33,7 +49,7 @@ exports.read_a_task = function(req, res) {
 
 
 exports.update_a_task = function(req, res) {
-  Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
+  Task.findOneAndUpdate({email:req.query.email, title:req.query.title}, req.body, {new: true}, function(err, task) {
     if (err)
       res.send(err);
     res.json(task);
@@ -43,7 +59,7 @@ exports.update_a_task = function(req, res) {
 
 exports.delete_a_task = function(req, res) {
   Task.remove({
-    _id: req.params.taskId
+    email:req.query.email, title:req.query.title
   }, function(err, task) {
     if (err)
       res.send(err);
--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -6,10 +6,18 @@ module.exports = function(app) {
   app.route('/tasks')
     .get(todoList.list_all_tasks);
 
+  app.route('/task/user')
+    .get(todoList.list_all_tasks_from_user)
+    .put(todoList.update_a_task)
+    .delete(todoList.delete_a_task);
+  
+  app.route('/task/user/pending')
+    .get(todoList.list_all_pending_tasks_from_user);
 
   app.route('/task')
     .post(todoList.create_a_task)
-    .get(todoList.read_a_task)
-    .put(todoList.update_a_task)
-    .delete(todoList.delete_a_task);
+    .get(todoList.read_a_task);
+
+  app.route('/task/user/done')
+    .post(todoList.update_a_task);
 };
